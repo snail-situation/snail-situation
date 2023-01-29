0df372c60e8a9afebd8658ac48eb51c8243f306a
nick thomas <nichoth@gmail.com>
2023-01-29 11:20:18 -0800

    add and build the hermes page

--- build.js
+++ build.js
@@ -1,8 +1,41 @@
-var hyperstream = require('hyperstream');
-var fs = require('fs');
+const hyperstream = require('hyperstream')
+const { mkdirp } = require('mkdirp')
+const fs = require('fs')
+var marked = require('marked')
 
-var hs = hyperstream({
+//
+// build /index.html
+//   css is piped to stdin
+//
+const hs = hyperstream({
     'style': process.stdin,
 })
-var rs = fs.createReadStream(__dirname + '/src/index.html')
+const rs = fs.createReadStream(__dirname + '/src/index.html')
 rs.pipe(hs).pipe(process.stdout)
+
+//
+// build sub paths
+//   - /hermes
+//
+fs.readFile(__dirname + '/src/hermes.md', 'utf8', (err, file) => {
+    if (err) throw err
+    const content = marked.parse(file)
+
+    const hermesHs = hyperstream({
+        'head': {
+            _appendHtml: `<link rel="stylesheet" href="/hermes.css">`
+        },
+        'title': 'snail situation | hermes',
+        'body': {
+            _appendHtml: `<div id="content">${content}</div>`
+        }
+    })
+    mkdirp.sync(__dirname + '/public/hermes')
+    const ws = fs.createWriteStream(__dirname + '/public/hermes/index.html')
+
+    // read an index.html template
+    fs.createReadStream(__dirname + '/src/_index.html')
+        .pipe(hermesHs)
+        .pipe(ws)
+})
+    
